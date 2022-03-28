714e94f99cbc5ba864143a8429c329dfd3ac0dd5
Mehmet <Mehmet@DESKTOP-FP1KRES>
2022-03-28 14:59:42 +0300

    win service signing duzeltme, isim duzeltme

--- Sultanlar.BayiServis/Sultanlar.BayiWinServis/ProjectInstaller.Designer.cs
+++ Sultanlar.BayiServis/Sultanlar.BayiWinServis/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@ namespace Sultanlar.BayiWinServis
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "Sultanlar - Bayi Servis";
+            this.serviceInstaller1.ServiceName = "Sultanlar Bayii Servis";
             // 
             // ProjectInstaller
             // 
